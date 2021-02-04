ef8fab8e89f14ddf8ee9f6913287180b08536ab5
Atul Anand <atulcleaver@gmail.com>
2021-02-04 08:57:22 +0530

    add this game file made of python.

---
+++ guess_game.py
@@ -0,0 +1,16 @@
+
+secret_word = "Atul"
+guess = ""
+chances = 3
+
+while guess != secret_word and chances != 0:
+    guess = input("What's your guess: ")
+    if guess != secret_word and chances != 0:
+        chances -= 1
+        if chances > 0:
+            print("It's wrong...")
+            print("Continue, you have only ", chances, " chances left.")
+    if guess != secret_word and chances == 0:
+        print("You lost dude! Run the program again and try again!!")
+    elif guess == secret_word:
+        print("You win!!")
